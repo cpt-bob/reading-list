9f14fd366c21d3ba4d6f34e0739cef7c9050af69
ltbob <cpt.bob@gmail.com>
2024-06-25 13:44:42 -0600

    Finshed My Reading list app

---
+++ script.js
@@ -0,0 +1,47 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Please enter a task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let close = document.createElement("close");
+    close.innerHTML = "&#215;";
+    li.appendChild(close);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.tagName === "LI") {
+      event.target.classList.toggle("checked");
+      saveData();
+    } else if (event.target.tagName === "CLOSE") {
+      event.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTaskList() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTaskList();
+
+document.body.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
